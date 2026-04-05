fec9cd29e586e6a449a796d24b6fadb9ea1295b1
小白 <boosoyzz@gmail.com>
2026-04-05 16:33:12 +0800

    Update backup.py

--- scripts/backup.py
+++ scripts/backup.py
@@ -89,7 +89,7 @@ def upload_to_webdav(backup_path):
     
     upload_url = WEBDAV_URL.rstrip('/') + '/' + backup_path.name
     
-    print(f"📤 上传到 WebDAV: {upload_url}")
+    print(f"📤 上传到 WebDAV")
     
     try:
         with open(backup_path, 'rb') as f:
