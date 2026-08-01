a4488ae6d3e7cd8b1798e86474a3601e6acf1e5a
小白 <boosoyzz@gmail.com>
2026-08-02 01:26:50 +0800

    Update update-db.js

--- scripts/update-db.js
+++ scripts/update-db.js
@@ -216,7 +216,8 @@ async function queryFreeIP(ip) {
 // =============================================
 // 冷门区域列表（使用双源验证，优先使用 freeipapi）
 // =============================================
-const COLD_REGIONS = ['NU', 'TK', 'PN', 'EH', 'CX', 'CC', 'NF', 'CK', 'WF', 'PM', 'SH', 'GS', 'AQ', 'BV', 'HM', 'TF', 'XK', 'KP', 'GL'];
+// const COLD_REGIONS = ['NU', 'TK', 'PN', 'EH', 'CX', 'CC', 'NF', 'CK', 'WF', 'PM', 'SH', 'GS', 'AQ', 'BV', 'HM', 'TF', 'XK', 'KP', 'GL'];
+const COLD_REGIONS = ['NU', 'TK', 'PN', 'CX', 'CC', 'NF', 'CK', 'WF', 'PM', 'SH', 'GS', 'AQ', 'BV', 'HM', 'TF', 'XK', 'KP', 'GL'];
 
 async function verifyRegionIP(ip, cc) {
   let actual = null;
